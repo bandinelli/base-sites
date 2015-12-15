bc7f0a8c1c87a89855f52c2b6b44ce724ff46e6e
bandinelli <erkweb@hotmail.com>
2015-12-14 22:45:54 -0200

    base site

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html ng-app="app" lang="pt-Br">
 <head>
-  <title>Clínica Moi</title>
+  <title>Nome Site</title>
   <meta charset="UTF-8">
-  <meta name="description" content="Clínica Moi" />
-  <meta name="keywords" content="Clínica Moi" />
+  <meta name="description" content="Nome Site" />
+  <meta name="keywords" content="Nome Site" />
   <meta name="author" content="ODNET / +55 (51) 3707-0107" />
   <meta name="google-site-verification" content=""/>
   <meta name="robots" content="all"/>
@@ -17,10 +17,10 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta property="og:title" content="Clínica Moi" />
-  <meta property="og:url" content="http://clinicamoi.com.br" />
+  <meta property="og:title" content="Nome Site" />
+  <meta property="og:url" content="http://urlsite.com.br" />
   <meta property="og:locale" content="pt-BR" />
-  <meta property="og:image" content="http://clinicamoi.com.br/img/">
+  <meta property="og:image" content="http://urlsite.com.br/img/">
   <meta property="og:image:type" content="image/png">
 
   <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
